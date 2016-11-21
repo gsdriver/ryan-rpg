ac999e033576ce5388bc096aa3db2fab254d67d0
Garrett Vargas <gsdriver@live.com>
2016-11-21 10:31:06 -0800

    Skill framework

---
+++ src/index.js
@@ -0,0 +1,127 @@
+/*
+ * MIT License
+
+ * Copyright (c) 2016 Garrett Vargas
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+'use strict';
+
+var AlexaSkill = require('./AlexaSkill');
+var storage = require('./storage');
+
+var APP_ID = undefined;
+
+var RyanRPG = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+RyanRPG.prototype = Object.create(AlexaSkill.prototype);
+RyanRPG.prototype.constructor = RyanRPG;
+
+RyanRPG.prototype.eventHandlers.onLaunch = function (launchRequest, session, response)
+{
+    var speechText = "Welcome to Ryan's Role Playing Game. How can I help you?";
+    
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    var repromptText = "For instructions on what you can say, please say help me.";
+
+    response.ask(speechText, repromptText);
+};
+
+RyanRPG.prototype.intentHandlers = {
+    // Stop intent
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+    // Cancel intent - for now we are session-less so does the same as goodbye
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+    // Help intent - provide help
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "You can play Ryan's Role Playing Game ... Now, what can I help you with?";
+        var repromptText = "You can play Ryan's Role Playing Game ... Now, what can I help you with?";
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+};
+
+/*
+ * Sends a response to Alexa - you would expect one of speechError,
+ * speechResponse, or speechQuestion to be set.  repromptQuestion
+ * will be set if speechQuestion is set and will be a shorter form
+ * of the speechQuestion (just asking what they want to do rather than
+ * giving a full game status)
+ */
+function SendAlexaResponse(speechError, speechResponse, speechQuestion, repromptQuestion, response)
+{
+    var speechOutput;
+    var repromptOutput;
+    var cardTitle = "Restaurant Finder";
+
+    if (speechError)
+    {
+        speechOutput = {
+            speech: speechError,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        repromptOutput = {
+            speech: "What else can I help with?",
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    }
+    else if (speechResponse) {
+        speechOutput = {
+            speech: speechResponse,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.tellWithCard(speechOutput, cardTitle, speechResponse);
+    }
+    else {
+        speechOutput = {
+            speech: speechQuestion,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        repromptOutput = {
+            speech: repromptQuestion,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.askWithCard(speechOutput, repromptOutput, cardTitle, speechQuestion);
+    }
+}
+
+exports.handler = function (event, context) 
+{
+    var rpg = new RyanRPG();
+    rpg.execute(event, context);
+};
